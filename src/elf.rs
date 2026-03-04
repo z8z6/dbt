dda3011dc2834bea81cb9493300bf0e9477c801e
z8z6 <zhou_zhengming@foxmail.com>
2026-03-04 22:11:48 +0800

    [dev] init elf

---
+++ src/elf.rs
@@ -0,0 +1,11 @@
+pub fn header(s: &str) -> String{
+    s.to_string() + ": elf header"
+}
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn x86_64(){
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod elf;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file = &args[1];
+    let header = elf::header(file);
+    println!("{}", header);
 }
